02db4e46c45aeab719a47dbd52a376caa4049ecf
Anand <anand.shenoy14@gmail.com>
2019-08-24 11:58:59 -0700

    adding some logging

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.get('/config', (req, res) => {
       "FIRESTORE_KEY" : "something_else",
       "MY_KEY" : "something"
   }
+  console.log(`My Config is ${process.env.BGK_FIRESTORE}`);
   res.send(configs);
 });
 
-app.listen(3000, () =>
-  console.log('Config App listening on port 3000!'),
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Config App listening on port : '+process.env.PORT),
 );
