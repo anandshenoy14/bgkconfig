7252a030042dec6ba2e73c5197c36a00cdcbdf53
Anand <anand.shenoy14@gmail.com>
2019-08-24 18:43:55 -0700

    get all configs

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-let express  = require('express');
+let express = require('express');
 let dotenv = require("dotenv")
 let cors = require("cors")
 const app = express();
-
+const CONFIG_PREFIX = "BGK"
 app.use(cors());
 
 app.get('/config', (req, res) => {
-  const configs = Object.assign({},process.env.BGK_CONFIGS);
-  res.json(configs);
+    const allConfigKeys = Object.keys(process.env).filter(k => k.indexOf(CONFIG_PREFIX) !== -1);
+    if(allConfigKeys.length > 0){
+        const allConfigs = {};
+        allConfigKeys.forEach(k => allConfigs[k] = process.env[`${CONFIG_PREFIX}_${k}`])
+        res.json(allConfigs);
+    }else{
+        res.json({"error" : "No Config Found"})
+    }    
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log('Config App listening on port : '+process.env.PORT),
+    console.log('Config App listening on port : ' + process.env.PORT),
 );
